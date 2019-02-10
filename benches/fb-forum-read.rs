32c3858d521fd8b3fbf426e80921b53592c3cb9c
Piotr Bielak <piotrbielak2@gmail.com>
2019-02-11 00:26:17 +0100

    Added first benchmark test

---
+++ benches/fb-forum-read.rs
@@ -0,0 +1,20 @@
+#[macro_use]
+extern crate criterion;
+
+use std::path::PathBuf;
+
+use criterion::Criterion;
+
+use rusty_nets::reader::*;
+
+
+fn bench_fb_forum_read(c: &mut Criterion) {
+    c.bench_function("fb-forum read", move |b| b.iter(|| {
+        let reader = EdgeListReader::new(',');
+        let path: PathBuf = "resources/fb-forum.txt".parse().unwrap();
+        reader.read(path).unwrap()
+    }));
+}
+
+criterion_group!(benches, bench_fb_forum_read);
+criterion_main!(benches);
