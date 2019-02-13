e839bf709576e4ae6e95858e5abb75c7d09e4ee4
Piotr Bielak <piotrbielak2@gmail.com>
2019-02-13 19:25:05 +0100

    Added benchmark for embedding loading

--- benches/fb-forum-read.rs
+++ benches/fb-forum-read.rs
@@ -5,16 +5,29 @@ use std::path::PathBuf;
 
 use criterion::Criterion;
 
+use rusty_nets::embedding::reader::*;
 use rusty_nets::network::reader::*;
 
-
 fn bench_fb_forum_read(c: &mut Criterion) {
-    c.bench_function("fb-forum read", move |b| b.iter(|| {
-        let reader = EdgeListReader::new(',');
-        let path: PathBuf = "resources/nets/fb-forum.txt".parse().unwrap();
-        reader.read(path).unwrap()
-    }));
+    c.bench_function("fb-forum read", move |b| {
+        b.iter(|| {
+            let reader = EdgeListReader::new(',');
+            let path: PathBuf = "resources/nets/fb-forum.txt".parse().unwrap();
+            reader.read(path).unwrap()
+        })
+    });
 }
 
-criterion_group!(benches, bench_fb_forum_read);
+fn bench_fb_forum_embedding_read(c: &mut Criterion) {
+    c.bench_function("fb-forum embedding read", move |b| {
+        b.iter(|| {
+            let reader = W2VEmbeddingVectorsReader::new();
+            let path: PathBuf = "resources/embs/fb-forum-embedding.txt".parse().unwrap();
+
+            reader.read(path).unwrap()
+        })
+    });
+}
+
+criterion_group!(benches, bench_fb_forum_read, bench_fb_forum_embedding_read);
 criterion_main!(benches);
